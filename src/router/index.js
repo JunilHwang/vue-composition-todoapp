0bdb72a40c8e7d9e827adb31e3ee6e76a3170b17
JunilHwang <junil.h@kakao.com>
2020-11-30 16:30:00 +0900

    feat: scss 초기화 완료

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { Step1, Step2, Step3 } from "../views";
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: "/step1",
+    name: "Step1",
+    component: Step1
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/step2",
+    name: "Step2",
+    component: Step2
+  },
+  {
+    path: "/step3",
+    name: "Step3",
+    component: Step3
+  },
 ];
 
 const router = createRouter({

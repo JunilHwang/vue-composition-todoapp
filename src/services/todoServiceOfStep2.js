e821742542797666e8cf18bcfe6db883287343ca
junilHwang <junil.h@kakao.com>
2020-12-02 00:10:49 +0900

    fix: API Path 오류 수정

--- src/services/todoServiceOfStep2.js
+++ src/services/todoServiceOfStep2.js
@@ -4,30 +4,30 @@ export const fetchUsers = () => todoClient.get("/users");
 
 export const addUser = name => todoClient.post("/users", { name });
 
-export const fetchUser = userId => todoClient.get(`/user/${userId}`);
+export const fetchUser = userId => todoClient.get(`/users/${userId}`);
 
-export const removeUser = userId => todoClient.delete(`/user/${userId}`);
+export const removeUser = userId => todoClient.delete(`/users/${userId}`);
 
 export const fetchItemsByUser = userId =>
-  todoClient.get(`/user/${userId}/items`);
+  todoClient.get(`/users/${userId}/items`);
 
 export const addItemByUser = (userId, contents) =>
-  todoClient.post(`/user/${userId}/items`, { contents });
+  todoClient.post(`/users/${userId}/items`, { contents });
 
 export const removeUserItems = userId =>
-  todoClient.delete(`/user/${userId}/items`);
+  todoClient.delete(`/users/${userId}/items`);
 
 export const removeUserItemById = (userId, itemId) =>
-  todoClient.delete(`/user/${userId}/items/${itemId}`);
+  todoClient.delete(`/users/${userId}/items/${itemId}`);
 
 export const updateItemByUserIdAndItemId = (userId, itemId, contents) =>
-  todoClient.put(`/user/${userId}/items/${itemId}`, { contents });
+  todoClient.put(`/users/${userId}/items/${itemId}`, { contents });
 
 export const updateItemPriorityByUserIdAndItemId = (userId, itemId, priority) =>
-  todoClient.put(`/user/${userId}/items/${itemId}/priority`, { priority });
+  todoClient.put(`/users/${userId}/items/${itemId}/priority`, { priority });
 
 export const toggleItemByUserIdAndItemId = (userId, itemId) =>
-  todoClient.put(`/user/${userId}/items/${itemId}/toggle`);
+  todoClient.put(`/users/${userId}/items/${itemId}/toggle`);
 
 export default {
   fetchUsers,

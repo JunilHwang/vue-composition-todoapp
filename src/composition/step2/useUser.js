ade479d0b8b17e9b9233d4718e49e7f7d7783388
junilHwang <junil.h@kakao.com>
2020-12-03 23:34:33 +0900

    refactor: step2 useUser에 store 적용 완료

--- src/composition/step2/useUser.js
+++ src/composition/step2/useUser.js
@@ -1,36 +1,28 @@
-import { reactive, toRefs } from "@vue/reactivity";
 import todoServiceOfStep2 from "@/services/todoServiceOfStep2";
+import useStoreModuleMapper from "@/composition/store/useStoreModuleMapper";
+import { ADD_USER, FETCH_USERS, REMOVE_USER, SET_USER } from "@/store/step2";
 
 export default function useUser() {
-  const state = reactive({
-    users: [],
-    selectedUserId: null
-  });
-
-  const fetchUsers = async () => {
-    const users = await todoServiceOfStep2.fetchUsers();
-    state.users = users.map(({ _id, name }) => ({ id: _id, name }));
-  };
-
-  const selectUser = id => {
-    state.selectedUserId = id;
-  };
-
-  const addUser = async name => {
-    const { _id } = await todoServiceOfStep2.addUser(name);
-    await fetchUsers();
-    state.selectedUserId = _id;
-  };
-
-  const removeUser = async () => {
-    await todoServiceOfStep2.removeUser(state.selectedUserId);
-    await fetchUsers();
-  };
+  const {
+    mapState,
+    mapGetters,
+    mapActions,
+    mapMutations
+  } = useStoreModuleMapper("step2");
+  const [selectedUserId] = mapState(["selectedUserId"]);
+  const [selectedUser] = mapGetters(["selectedUser"]);
+  const [selectUser] = mapMutations([SET_USER]);
+  const [fetchUsers, addUser, removeUser] = mapActions([
+    FETCH_USERS,
+    ADD_USER,
+    REMOVE_USER
+  ]);
 
   return {
-    ...toRefs(state),
-    fetchUsers,
+    selectedUserId,
+    selectedUser,
     selectUser,
+    fetchUsers,
     addUser,
     removeUser
   };

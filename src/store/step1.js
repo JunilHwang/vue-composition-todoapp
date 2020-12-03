7d02143c098d9d618e9be1fb3c6f74cbeadc142d
junilHwang <junil.h@kakao.com>
2020-12-03 11:21:05 +0900

    feat: step1 store 작성 완료

--- src/store/step1.js
+++ src/store/step1.js
@@ -1,9 +1,26 @@
+import todoServiceOfStep1 from "@/services/todoServiceOfStep1";
+
+export const SET_TODO_ITEMS = "SET_TODO_ITEMS";
+
 const step1 = {
-  state: {},
+  state: {
+    todoItems: todoServiceOfStep1.fetchTodoItems().reduce(
+      (obj, item) => {
+        obj.entities[item.id] = item;
+        obj.ids.push(item.id);
+        return obj;
+      },
+      { entities: {}, ids: [] }
+    ),
+  },
 
-  mutations: {},
-
-  actions: {}
+  mutations: {
+    [SET_TODO_ITEMS](state, todoItems) {
+      state.todoItems = todoItems;
+      const { ids, entities } = todoItems;
+      todoServiceOfStep1.putTodoItems(ids.map(id => entities[id]));
+    }
+  }
 };
 
 export default step1;
